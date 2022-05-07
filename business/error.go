83560345bb2d4210784bbdf562b361f6f2da847f
dhichii <adhicittamasran@gmail.com>
2022-05-07 21:46:57 +0800

    Add error bad request

--- business/error.go
+++ business/error.go
@@ -3,6 +3,8 @@ package business
 import "errors"
 
 var (
+	// Error when there is bad request
+	ErrBadRequest = errors.New("bad request")
 	// Error when data is not found
 	ErrNotFound = errors.New("data was not found")
 	// Error when there is duplicate data or column
