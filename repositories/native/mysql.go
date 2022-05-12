5037be16880403d62b4f4de84e50dd00ef774b16
dhichii <adhicittamasran@gmail.com>
2022-05-12 18:16:33 +0800

    Create mysql native repo

---
+++ repositories/native/mysql.go
@@ -0,0 +1,51 @@
+package native
+
+import (
+	"plant-api/business/native"
+
+	"gorm.io/gorm"
+)
+
+type repository struct {
+	db *gorm.DB
+}
+
+// Generate mysql DB repository
+func NewMysqlRepository(db *gorm.DB) *repository {
+	return &repository{db}
+}
+
+// Create new native and store into database
+func (repo *repository) Create(native *native.Native) error {
+	if err := repo.db.Create(&native).Error; err != nil {
+		return err
+	}
+	return nil
+}
+
+// Get all natives
+func (repo *repository) GetAll() ([]native.Native, error) {
+	natives := []native.Native{}
+	if err := repo.db.Find(&natives).Error; err != nil {
+		return nil, err
+	}
+	return natives, nil
+}
+
+// Get native by given id. It's return nil if not found
+func (repo *repository) Get(id int) (*native.Native, error) {
+	native := native.Native{}
+	if err := repo.db.First(&native, id).Error; err != nil {
+		return nil, err
+	}
+	return &native, nil
+}
+
+// Get native by given name. It's return nil if not found
+func (repo *repository) GetByName(name string) (*native.Native, error) {
+	native := native.Native{}
+	if err := repo.db.Where("name", name).First(&native).Error; err != nil {
+		return nil, err
+	}
+	return &native, nil
+}
