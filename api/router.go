36731145b84882dd59368f6f8546f22825db984c
dhichii <adhicittamasran@gmail.com>
2022-05-13 14:52:39 +0800

    Add plant routes

--- api/router.go
+++ api/router.go
@@ -4,6 +4,7 @@ import (
 	authController "plant-api/api/v1/auth"
 	userV1Controller "plant-api/api/v1/user"
 	nativeV1Controller "plant-api/api/v1/native"
+	plantV1Controller "plant-api/api/v1/plant"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -13,6 +14,7 @@ type Controller struct {
 	UserV1Controller *userV1Controller.Controller
 	AuthController   *authController.Controller
 	NativeController *nativeV1Controller.Controller
+	PlantController *plantV1Controller.Controller
 }
 
 func InitRouter(e *echo.Echo, controller Controller, jwtSecret string) {
@@ -31,4 +33,11 @@ func InitRouter(e *echo.Echo, controller Controller, jwtSecret string) {
 	// Native route
 	v1.POST("/natives", controller.NativeController.Create)
 	v1.GET("/natives", controller.NativeController.GetAll)
+
+	// Plant route
+	v1.POST("/plants", controller.PlantController.Create)
+	v1.GET("/plants", controller.PlantController.GetAll)
+	v1.GET("/plants/:id", controller.PlantController.GetDetail)
+	v1.PUT("/plants/:id", controller.PlantController.Update)
+	v1.DELETE("/plants/:id", controller.PlantController.Delete)
 }
