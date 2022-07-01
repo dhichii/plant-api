a7551f949294a9d384d97ab6410049d622209ea8
Adhicitta Masran <adhicittamasran@gmail.com>
2022-07-01 18:29:00 +0800

    refactor: remove password from response

--- api/v1/user/response/response.go
+++ api/v1/user/response/response.go
@@ -2,9 +2,8 @@ package response
 
 // Create response payload
 type User struct {
-	ID       uint   `json:"id"`
-	Name     string `json:"name"`
-	Email    string `json:"email"`
-	Password string `json:"password"`
-	Role     string `json:"role"`
+	ID    uint   `json:"id"`
+	Name  string `json:"name"`
+	Email string `json:"email"`
+	Role  string `json:"role"`
 }
