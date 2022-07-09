1409659d89c68f040a19895c89c6866e66e796d9
Adhicitta Masran <adhicittamasran@gmail.com>
2022-07-09 23:19:13 +0800

    refactor: implement match password

--- business/auth/services.go
+++ business/auth/services.go
@@ -6,8 +6,7 @@ import (
 	"plant-api/business"
 	"plant-api/business/user"
 	"plant-api/config"
-
-	"golang.org/x/crypto/bcrypt"
+	"plant-api/utils"
 )
 
 type service struct {
@@ -33,8 +32,7 @@ func (s *service) Login(email, password string) (*response.Token, error) {
 		return nil, err
 	}
 	if user != nil {
-		matchPassword := matchPassword(user.Password, []byte(password))
-		if user.Email == email && matchPassword {
+		if utils.MatchPassword(user.Password, password) {
 			token, err := middleware.GenerateJWT(int(user.ID), user.Role, s.cfg.JWTSecret)
 			if err != nil {
 				return nil, business.ErrBadRequest
