0f973ddf871723ebccb72ad359870adb5aecad90
Adhicitta Masran <adhicittamasran@gmail.com>
2022-07-12 09:47:40 +0800

    feat(account): add to modules

--- app/modules/modules.go
+++ app/modules/modules.go
@@ -2,16 +2,19 @@ package modules
 
 import (
 	"plant-api/api"
+	accountController "plant-api/api/v1/account"
 	authController "plant-api/api/v1/auth"
 	nativeController "plant-api/api/v1/native"
 	plantController "plant-api/api/v1/plant"
 	userController "plant-api/api/v1/user"
+	accountService "plant-api/business/account"
 	authService "plant-api/business/auth"
 	nativeService "plant-api/business/native"
 	plantService "plant-api/business/plant"
 	plantNativeService "plant-api/business/plant_native"
 	userService "plant-api/business/user"
 	"plant-api/config"
+	accountRepository "plant-api/repositories/account"
 	nativeRepository "plant-api/repositories/native"
 	plantRepository "plant-api/repositories/plant"
 	plantNativeRepository "plant-api/repositories/plant_native"
@@ -26,6 +29,11 @@ func RegisterModules(db *gorm.DB, cfg config.Config) api.Controller {
 	userRepo := userRepository.NewMysqlRepository(db)
 	userService := userService.NewService(userRepo)
 	userController := userController.NewController(userService)
+
+	// Initiate account
+	accountRepo := accountRepository.NewMysqlRepository(db)
+	accountService := accountService.NewService(accountRepo)
+	accountController := accountController.NewController(accountService)
 
 	// Initiate auth
 	authService := authService.NewService(userRepo, cfg)
@@ -47,10 +55,11 @@ func RegisterModules(db *gorm.DB, cfg config.Config) api.Controller {
 
 	// Put all controllers together
 	controllers := api.Controller{
-		UserV1Controller: userController,
-		AuthController:   authController,
-		NativeController: nativeController,
-		PlantController:  plantController,
+		UserV1Controller:  userController,
+		AccountController: accountController,
+		AuthController:    authController,
+		NativeController:  nativeController,
+		PlantController:   plantController,
 	}
 	return controllers
 }
